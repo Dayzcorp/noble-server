524052a1c73ba7ca5bc274f4e172bfdd91894b24
Dayzcorp <tharreojani66@gmail.com>
2025-06-14 15:12:21 +0200

    add newline at EOF

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from openai import OpenAI
 
 load_dotenv()
 
-def get_shopify_products():
+def get_shopify_products(shop_domain: str):
 
-    shop_domain = os.getenv("SHOP_DOMAIN")
     storefront_token = os.getenv("SHOPIFY_STOREFRONT_TOKEN")
     url = f"https://{shop_domain}/api/2023-10/graphql.json"
 
